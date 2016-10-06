935e0437d25f44634739ca1249cbeb1c98fb8757
Avik Jain <avikjain12345@gmail.com>
2016-10-06 12:15:02 -0700

    Add correct input counter to data-collection

--- data-collection/public/js/logger.js
+++ data-collection/public/js/logger.js
@@ -1,12 +1,15 @@
 $(function() {
 	var timeDiffs = [];
 	var prevTime;
+    var counter = 0;
 	$('#input-field').on('keydown', function(event) {
 		var text = $(this).val()
         var key = event.which || event.keyCode || event.charCode;
         if(key == 8 || key == 46 || key == 13) {
-        	if(key == 13 && text == 'avikjain') {
-        		$.post('/data', { data: timeDiffs })
+        	if(key == 13 && text == 'avikjain' && timeDiffs.length == 7) {
+        		$.post('/data', { data: timeDiffs });
+                counter++;
+                $('#counter').html(counter+'');
         	}
         	timeDiffs = [];
         	prevTime = null;
